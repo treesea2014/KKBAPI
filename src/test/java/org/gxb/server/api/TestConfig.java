4c8657bbcae3b9fb9a161fda26ca7049d37dfd0f
treesea2014 <2117999344@qq.com>
2015-11-17 16:47:55 +0800

    config修改

--- src/test/java/org/gxb/server/api/TestConfig.java
+++ src/test/java/org/gxb/server/api/TestConfig.java
@@ -16,136 +16,60 @@ import net.sf.json.JSONObject;
  * ws.wang
  */
 public class TestConfig {
-
-//    public static final String path="http://w-api-r1.kaikeba.cn";
-//    public static final String path="http://release-api.kaikeba.cn";
 	public static ResourceBundle bundle = ResourceBundle.getBundle("api");
-	//请求地址
-    public static final String path = bundle.getString("env");
-    public static final String userid = bundle.getString("userid");
-    public static final String tenantid = bundle.getString("tenantid");
-    public static final String cid = bundle.getString("cid");
+	// 请求地址
+	public static final String path = bundle.getString("env");
+	public static final String basePath = "/gxb-api/";
 
-    
-    
-	
+	public static RequestSpecification requestSpecification() {
+		if (path.contains("cn")) {
+			return given().baseUri(path).and().header("Content-Type", "application/json;charset=utf-8").port(8080).basePath(basePath);
 
-    public static RequestSpecification requestSpecification(){
-        if(path.contains("cn")){
-            return given()
-                    .baseUri(path).and()
-                    .header("Content-Type","application/json;charset=utf-8")
-                    .port(8080);
+		} else {
+			return given().baseUri(path).and().header("Content-Type", "application/json").port(8080).basePath(basePath);
+		}
+	}
 
-        }else {
+	public static Response getOrDeleteExecu(String type, String url) {
+		if (type.equalsIgnoreCase("get")) {
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().contentType(ContentType.JSON);
 
-            return given()
-                    .baseUri(path).and()
-                    .header("Content-Type","application/json")
-                    .port(8080);
-        }
-    }
+			Response response = requestSpecification.when().get(url);
+			return response;
+		} else {
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().contentType(ContentType.JSON);
 
-    public static String getToken(String path){
-        ///kauth/authorize?uid=812277&cid=www&tenant_id=0
-        RequestSpecification requestSpecification= TestConfig.requestSpecification();
+			Response response = requestSpecification.when().delete(url);
+			return response;
+		}
+	}
 
-        Response response=requestSpecification.when()
-                .get(path);
+	public static Response postOrPutExecu(String type, String url, JSONObject jsonObject) {
+		if (type.equalsIgnoreCase("post")) {
 
-        String token=with(response.body().asString()).get("access_token");
-        return token;
-    }
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().body(jsonObject);
+			Response response = requestSpecification.when().post(url);
+			return response;
+		} else {
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().body(jsonObject);
 
-    public static String getTokenbyUserID(){
-        ///kauth/authorize?uid=812277&cid=www&tenant_id=0
+			Response response = requestSpecification.when().put(url);
+			return response;
+		}
+	}
 
-        //String user_name="fanqi2015";
-        //String email="52282661";
-        //String password="52282661";
+	public static Response postOrPutFileExecu(String type, String url, Object file) {
+		if (type.equalsIgnoreCase("post")) {
 
-        Integer user_id=632467; //普通用户
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().body(file);
+			Response response = requestSpecification.when().post(url);
+			return response;
+		} else {
+			RequestSpecification requestSpecification = TestConfig.requestSpecification().body(file);
 
-        RequestSpecification requestSpecification= TestConfig.requestSpecification();
+			Response response = requestSpecification.when().put(url);
+			return response;
+		}
+	}
 
-        String path="/kauth/authorize?uid=" + user_id.toString() + "&cid=www&tenant_id=1";
-
-        Response response=TestConfig.getOrDeleteExecu("get", path);
-
-        String token=with(response.body().asString()).get("access_token");
-        return token;
-    }
-
-    public static String getTokenByUsidAndTeid(int user_id, int tenants_id){
-
-        RequestSpecification requestSpecification= TestConfig.requestSpecification();
-
-        String path="/kauth/authorize?uid=" + user_id + "&cid=www&tenant_id=" + tenants_id;
-
-        Response response=TestConfig.getOrDeleteExecu("get", path);
-
-        String token=with(response.body().asString()).get("access_token");
-        
-        return token;
-    }
-    /**
-     * 获取默认token
-     * @return
-     */
-    public static String getDefaultToken(){
-        String path="/kauth/authorize?uid=" + userid + "&cid="+cid+"&tenant_id=" + tenantid;
-        Response response=TestConfig.getOrDeleteExecu("get", path);
-        //response.then().assertThat().log().all();
-        String token=with(response.body().asString()).get("access_token");
-        return token;
-    }
-    
-    public static Response getOrDeleteExecu(String type,String url){
-        if(type.equalsIgnoreCase("get")){
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().contentType(ContentType.JSON);
-
-            Response response=requestSpecification.when()
-                    .get(url);
-            return response;
-        }else{
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().contentType(ContentType.JSON);
-            
-            Response response=requestSpecification.when()
-                    .delete(url);
-            return response;
-        }
-    }
-
-    public static Response postOrPutExecu(String type,String url,JSONObject jsonObject){
-        if(type.equalsIgnoreCase("post")){
-
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().body(jsonObject);
-            Response response=requestSpecification.when()
-                    .post(url);
-            return response;
-        }else{
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().body(jsonObject);
-
-            Response response=requestSpecification.when()
-                    .put(url);
-            return response;
-        }
-    }
-
-    public static Response postOrPutFileExecu(String type,String url,Object file){
-        if(type.equalsIgnoreCase("post")){
-
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().body(file);
-            Response response=requestSpecification.when()
-                    .post(url);
-            return response;
-        }else{
-            RequestSpecification requestSpecification= TestConfig.requestSpecification().body(file);
-
-            Response response=requestSpecification.when()
-                    .put(url);
-            return response;
-        }
-    }
-    
 }
