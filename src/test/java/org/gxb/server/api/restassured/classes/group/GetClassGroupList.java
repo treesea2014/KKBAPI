5a1f3b0bee3a347ebadafd7c7a66406aabedfeed
treesea2014 <2117999344@qq.com>
2016-01-20 18:24:23 +0800

    修改

--- src/test/java/org/gxb/server/api/restassured/classes/group/GetClassGroupList.java
+++ src/test/java/org/gxb/server/api/restassured/classes/group/GetClassGroupList.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
  * Created by treesea on 16/1/20.
  */
 public class GetClassGroupList {
-    @Test
+    @Test(description = "正常" ,priority = 1)
     public void test(){
         Response response = TestConfig.getOrDeleteExecu("get",
                 "/class_group?filter=classId:1,query:班组&paging=curPage:1,pageSize:2");
@@ -19,7 +19,23 @@ public class GetClassGroupList {
                 .body("filter.query", Matchers.equalTo("班组"))
                 .body("filter.classId", Matchers.equalTo("1"))
                 .body("dataList.groupName", Matchers.hasItem(Matchers.containsString("班组")))
-                .body("dataList.teacherUserList.name", Matchers.equalTo("张金霞"))
+                .body("dataList.teacherUserList.name", Matchers.hasItem(Matchers.hasItem("张金霞")))
         ;
     }
+    @Test(description = "没有filter" ,priority = 2)
+    public void testWithoutFilter(){
+        Response response = TestConfig.getOrDeleteExecu("get",
+                "/class_group?paging=curPage:2,pageSize:50");
+        response.then().log().all().assertThat().statusCode(200)
+                .body("paging.curPage", Matchers.equalTo(2))
+                .body("paging.pageSize", Matchers.equalTo(50))
+                //.body("filter.query", Matchers.equalTo("班组"))
+                //.body("filter.classId", Matchers.equalTo("1"))
+                .body("dataList.groupName", Matchers.hasItem(Matchers.containsString("默认分组")))
+                //.body("dataList.teacherUserList.name", Matchers.hasItem(Matchers.hasItem("默认分组")))
+        ;
+    }
+
+
+
 }
